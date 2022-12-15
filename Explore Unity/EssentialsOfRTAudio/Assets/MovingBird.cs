37b6ac9a47ddbe7ebfd7ca6f33fbdc1015a30f52
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-12-15 14:16:36 -0800

    Add docs to moving bird script

--- Explore Unity/EssentialsOfRTAudio/Assets/MovingBird.cs
+++ Explore Unity/EssentialsOfRTAudio/Assets/MovingBird.cs
@@ -2,27 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum Movement { Up, Down } 
+/// <summary>
+/// The direction of which the the bird may move in.
+/// Author: Lukasz Bednarek
+/// Date: 2022-12-15
+/// </summary>
+public enum Direction { Up, Down }
 
+/// <summary>
+/// Responsible for moving the bird.
+/// Author: Lukasz Bednarek
+/// Date: 2022-12-15
+/// </summary>
 public class MovingBird : MonoBehaviour
 {
-    // various bird chirp audio clips
-    public AudioClip birdChirpOne;
-    public AudioClip birdChirpTwo;
-    public AudioClip birdChirpThree;
-
-    // 2 x coordinates bird will move from
+    // 2 x coordinates bird will move from.
     private float _upperXCoordinate = 10;
     private float _lowerXCoordinate = -10;
 
+    // the rate in which the position will change by.
     public Vector3 positionChange;
 
-    private Movement _moveDirection;
+    // the direction the bird is moving.
+    private Direction _moveDirection;
 
-    // Start is called before the first frame update
+    // Start is called before the first frame update.
     void Start()
     {
-        _moveDirection = Movement.Up;
+        _moveDirection = Direction.Up;
     }
 
     // Update is called once per frame
@@ -32,25 +39,35 @@ public class MovingBird : MonoBehaviour
         MoveBird();
     }
 
+    /// <summary>
+    /// Changes the x-coordinate direction in which the bird moves towards.
+    /// </summary>
     private void CheckBirdDirection()
     {
-        if (_moveDirection == Movement.Down && transform.position.x <= _lowerXCoordinate) ChangeDirection(Movement.Up);
-        else if (_moveDirection == Movement.Up && transform.position.x >= _upperXCoordinate) ChangeDirection(Movement.Down);
+        if (_moveDirection == Direction.Down && transform.position.x <= _lowerXCoordinate) ChangeDirection(Direction.Up);
+        else if (_moveDirection == Direction.Up && transform.position.x >= _upperXCoordinate) ChangeDirection(Direction.Down);
         else return;
     }
 
-    private void ChangeDirection(Movement newDirection)
+    /// <summary>
+    /// Changes the direction of the bird to the new direction
+    /// </summary>
+    /// <param name="newDirection"></param>
+    private void ChangeDirection(Direction newDirection)
     {
         _moveDirection = newDirection;
     }
 
+    /// <summary>
+    /// Moves the bird in a direction.
+    /// </summary>
     private void MoveBird()
     {
-        if (_moveDirection == Movement.Up)
+        if (_moveDirection == Direction.Up)
         {
             transform.position += positionChange;
         }
-        else if (_moveDirection == Movement.Down)
+        else if (_moveDirection == Direction.Down)
         {
             transform.position -= positionChange;
         }
---
+++ Explore Unity/EssentialsOfRTAudio/Assets/RandomBirdNoises.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomBirdNoises : MonoBehaviour
+{
+    // various bird chirp audio clips
+    public AudioClip birdChirpOne;
+    public AudioClip birdChirpTwo;
+    public AudioClip birdChirpThree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
