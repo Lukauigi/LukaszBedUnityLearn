5fceff36ab2b9f22e2d30242bce26aeab4cbf724
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2022-12-15 14:34:33 -0800

    Add random bird chirps

--- Explore Unity/EssentialsOfRTAudio/Assets/RandomBirdNoises.cs
+++ Explore Unity/EssentialsOfRTAudio/Assets/RandomBirdNoises.cs
@@ -9,15 +9,28 @@ public class RandomBirdNoises : MonoBehaviour
     public AudioClip birdChirpTwo;
     public AudioClip birdChirpThree;
 
+    private List<AudioClip> _birdChirpClips;
+    private AudioSource _birdChirpAudioSource;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _birdChirpClips = new List<AudioClip> { birdChirpOne, birdChirpTwo, birdChirpThree };
+        _birdChirpAudioSource = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!_birdChirpAudioSource.isPlaying)
+        {
+            // get a random chirp clip from the list
+            int index = Random.Range(0, _birdChirpClips.Count);
+            AudioClip birdChirp = _birdChirpClips[index];
+
+            // assign & play clip
+            _birdChirpAudioSource.clip = birdChirp;
+            _birdChirpAudioSource.Play();
+        }
     }
 }
