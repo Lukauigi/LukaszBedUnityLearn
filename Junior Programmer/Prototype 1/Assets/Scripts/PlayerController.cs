82f2078fb7e51127086ea41a8b21be0d781aec72
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-02-03 11:00:59 -0800

    Add obstacles to the scene with a self driving vehicle

---
+++ Junior Programmer/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the vehicle forward
+        // Gets the transform component of the current object & use the translate method to move the vehicle
+        // Vector3.forward is a cleaner way to use transform.Translate(0, 0, 1);
+        // Use deltatime to move the vehicle every second instead of frames that could be variable on different users' devices
+        transform.Translate(Vector3.forward * Time.deltaTime * 20); 
+    }
+}
