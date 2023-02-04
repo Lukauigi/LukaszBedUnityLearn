d572f18a6ee41b50d78615379f9ada4d2700b00a
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-02-03 16:00:19 -0800

    Fix plane speed & auto tilting

--- Junior Programmer/Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Junior Programmer/Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -20,10 +20,13 @@ public class PlayerControllerX : MonoBehaviour
         // get the user's vertical input
         verticalInput = Input.GetAxis("Vertical");
 
+        // Change Vector3.back to Vector3.forward to move to where the obstacles are
         // move the plane forward at a constant rate
         transform.Translate(Vector3.forward * speed);
 
+        // Need to multiply by verticalInput since no key press will be zero, which will not rotate the plane
+        // verticalInput will also dictate the rotation of the plane's x axis (tilting its nose)
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
