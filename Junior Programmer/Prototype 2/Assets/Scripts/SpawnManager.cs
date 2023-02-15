4285db03b9fe492b43921a12b6a928d8347b9197
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-02-15 01:18:57 -0800

    Spawn of animals are random along the x axis

--- Junior Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Junior Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -10,6 +10,12 @@ using UnityEngine;
 /// </summary>
 public class SpawnManager : MonoBehaviour
 {
+    // Private vars for spawning
+    private readonly float _spawnRangeXAxis = 20;
+    private readonly float _spawnPositionYAxis = 0;
+    private readonly float _spawnPositionZAxis = 20;
+
+    // Array of animal prefabs customized in editor
     public GameObject[] animalPrefabs;
 
     // Start is called before the first frame update
@@ -21,11 +27,14 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // Spawns a random animal on key press
+        // Spawns a random animal at a random x position along fixed Y & Z axises on key press
         if (Input.GetKeyDown(KeyCode.S))
         {
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(0, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            // randomized x axis position, fixed y and z axis positions
+            Vector3 spawnPosition = new Vector3(Random.Range(-_spawnRangeXAxis, _spawnRangeXAxis), _spawnPositionYAxis, _spawnPositionZAxis);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
         }
     }
 }
