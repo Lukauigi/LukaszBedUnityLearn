9f6ca6b52949cb30eeae2ead41667511247e5b98
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-02-16 14:06:02 -0800

    Fix spawn interval to be random each game

--- Junior Programmer/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Junior Programmer/Prototype 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,12 +11,15 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    //private float spawnInterval = 3;
+
+    private float _spawnLowerBoundInterval = 3;
+    private float _spawnUpperBoundInterval = 5;
 
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+        InvokeRepeating(nameof(SpawnRandomBall), startDelay, Random.Range(_spawnLowerBoundInterval, _spawnUpperBoundInterval));
     }
 
     // Spawn random ball at random x position at top of play area
