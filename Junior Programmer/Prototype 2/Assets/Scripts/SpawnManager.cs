688e7dda2aae5ae0431c7b4a9db78001ef60acc6
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-02-16 01:01:44 -0800

    Edit animal spawning to time intervals

--- Junior Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ Junior Programmer/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -14,6 +14,8 @@ public class SpawnManager : MonoBehaviour
     private readonly float _spawnRangeXAxis = 20;
     private readonly float _spawnPositionYAxis = 0;
     private readonly float _spawnPositionZAxis = 20;
+    private readonly float _startDelay = 2;
+    private readonly float _spawnInterval = 1.5f;
 
     // Array of animal prefabs customized in editor
     public GameObject[] animalPrefabs;
@@ -21,20 +23,27 @@ public class SpawnManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating(nameof(SpawnRandomAnimal), _startDelay, _spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
         // Spawns a random animal at a random x position along fixed Y & Z axises on key press
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            // randomized x axis position, fixed y and z axis positions
-            Vector3 spawnPosition = new Vector3(Random.Range(-_spawnRangeXAxis, _spawnRangeXAxis), _spawnPositionYAxis, _spawnPositionZAxis);
+        // if (Input.GetKeyDown(KeyCode.S)) SpawnRandomAnimal();
+    }
 
-            Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
-        }
+    /// <summary>
+    /// Instantiates a random animal from the collection of animal prefabs.
+    /// 
+    /// Date: 2023-02-16
+    /// </summary>
+    private void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        // randomized x axis position, fixed y and z axis positions
+        Vector3 spawnPosition = new Vector3(Random.Range(-_spawnRangeXAxis, _spawnRangeXAxis), _spawnPositionYAxis, _spawnPositionZAxis);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPosition, animalPrefabs[animalIndex].transform.rotation);
     }
 }
