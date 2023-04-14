cacd79d861ddbf839071f590eed8a39048247aa6
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-04-14 01:46:51 -0700

    Add jump input

--- Junior Programmer/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -1,18 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Component to control the player.
+/// </summary>
+/// 
+/// <remarks>
+/// Author: Lukasz Bednarek
+/// Date: 2023-04-14
+/// </remarks>
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    // Refs to component(s)
+    private Rigidbody playerRb;
+
+    ///<inheritdoc />
     void Start()
     {
-        
+        playerRb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse); //use impulse ForceMode to simulate an object suddenly receiving a push.
+        }
     }
 }
