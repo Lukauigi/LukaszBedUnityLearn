e9a18dea72f8194a0bf320cc185f7d89b6e35bcc
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-04-14 01:55:43 -0700

    Add jumpForce & gravity modifers to the player.

--- Junior Programmer/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Junior Programmer/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -10,6 +10,10 @@ using UnityEngine;
 /// </remarks>
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField, Tooltip("The force which propels the player into a jump.")]
+    private float _jumpForce;
+    [SerializeField, Tooltip("The gravity rate of the player.")]
+    private float _gravityModifier;
 
     // Refs to component(s)
     private Rigidbody playerRb;
@@ -18,6 +22,7 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= _gravityModifier; // Modifies the rate of the player's gravity.
     }
 
     // Update is called once per frame
@@ -25,7 +30,7 @@ public class PlayerController : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse); //use impulse ForceMode to simulate an object suddenly receiving a push.
+            playerRb.AddForce(Vector3.up * _jumpForce, ForceMode.Impulse); //use impulse ForceMode to simulate an object suddenly receiving a push.
         }
     }
 }
