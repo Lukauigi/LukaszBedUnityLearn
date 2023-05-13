ff2fc47a00273366bd8b1e2af20a0ad4db5f4237
Lukasz_Bednarek <lukebednarek@hotmail.ca>
2023-05-13 16:49:15 -0700

    Edit powerup type data

--- Junior Programmer/Prototype4GameplayMechanics/Assets/Scripts/Powerups/PowerupScriptableObject.cs
+++ Junior Programmer/Prototype4GameplayMechanics/Assets/Scripts/Powerups/PowerupScriptableObject.cs
@@ -15,9 +15,21 @@ public class PowerupScriptableObject : ScriptableObject
 {
     [SerializeField, Tooltip("Powerup name.")]
     private string _name;
+    [SerializeField, Tooltip("How long the powerup lasts.")]
+    private float _duration;
+    [SerializeField, Tooltip("The strength to push away enemies.")]
+    private float _knockback;
 
     /// <summary>
     /// The name of the powerup.
     /// </summary>
     public string Name { get { return _name; } }
+    /// <summary>
+    /// How long the powerup lasts.
+    /// </summary>
+    public float Duration { get { return _duration; } }
+    /// <summary>
+    /// The strength to push away enemies.
+    /// </summary>
+    public float Knockback { get { return _knockback; } }
 }
